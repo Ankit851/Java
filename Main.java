4bbe648f3ee3454fb18baae96de4c4332942ff6b
Ankit Mahajan <69541646+Ankit851@users.noreply.github.com>
2021-03-28 11:05:02 +0530

    ✨✨ Addition of 3 numbers ✨✨

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("The Sum of these Numbers are :- ");
+        int num1 = 34;
+        int num2 = 40;
+        int num3 = 12;
+        int sum  = num1 + num2 + num3;
+        System.out.println(sum);
 
     }
 }
